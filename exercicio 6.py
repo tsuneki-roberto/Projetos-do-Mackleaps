22447195246528ea3bb0e3269eea6d370e61a72d
tsuneki <contato.tsuneki@gmail.com>
2019-02-14 17:15:43 -0200

    Aprovado?

---
+++ exercicio 6.py
@@ -0,0 +1,11 @@
+x=float(input())
+y=float(input())
+z=float(input())
+w=float(input())
+
+if w>20:
+    print("Reprovado por falta!")
+elif (x+y+z)/3 >=7:
+    print("Aluno aprovado!")
+else:
+    print("Aluno reprovado por media insuficiente!")
