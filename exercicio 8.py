5c47f6161a694ea6672cf05da85c1a7483d7a36f
tsuneki <contato.tsuneki@gmail.com>
2019-02-14 17:16:26 -0200

    Quente ou frio?

---
+++ exercicio 8.py
@@ -0,0 +1,12 @@
+x=float(input())
+
+if x<15:
+    print("Muito frio")
+elif x<23:
+    print("Frio")
+elif x<26:
+    print("Agradavel")
+elif x<30:
+    print("Calor")
+else:
+    print("Muito quente")
